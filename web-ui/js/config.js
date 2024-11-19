a13c6a0b0802ec5fbc93c599eb09a6ce5e9007d8
lallym <mr.lally@gmail.com>
2024-11-19 11:26:18 -0500

    change region

--- web-ui/js/config.js
+++ web-ui/js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: '', // e.g. us-east-1_uXboG5pAb
         userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-1
+        region: 'us-west-2' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
